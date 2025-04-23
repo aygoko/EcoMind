651082e08a67ecc89bd9fa29eb02c92449ed848d
aygoko <112821716+aygoko@users.noreply.github.com>
2025-04-23 19:07:52 +0300

    Add CO2 param to user struct

--- backend/domain/user.go
+++ backend/domain/user.go
@@ -1,9 +1,8 @@
-package repository
-
 type User struct {
-	ID          string `json:"id"`
-	Login       string `json:"login"`
-	Email       string `json:"email"`
-	PhoneNumber string `json:"phone_number"`
-	Password    string `json:"-"`
+    ID          string  `json:"id"`
+    Login       string  `json:"login"`
+    Email       string  `json:"email"`
+    PhoneNumber string  `json:"phone_number"`
+    Password    string  `json:"-"`
+    CO2         float64 `json:"co2"` 
 }
